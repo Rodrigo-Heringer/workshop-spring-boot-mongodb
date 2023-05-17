ea16fab20b9e4fa41b4ca367374077dcca4c0320
Rodrigo-Heringer <rodrigo.heringer@yahoo.com.br>
2023-05-17 13:36:15 -0300

    Insercao de usuario com POST

--- src/main/java/com/rodrigoheringer/workshopmongo/services/UserService.java
+++ src/main/java/com/rodrigoheringer/workshopmongo/services/UserService.java
@@ -7,21 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigoheringer.workshopmongo.domain.User;
+import com.rodrigoheringer.workshopmongo.dto.UserDTO;
 import com.rodrigoheringer.workshopmongo.repository.UserRepository;
 import com.rodrigoheringer.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
 
     public User findById(String id) {
-        Optional<User> obj = repo.findById(id);        
+        Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-        }
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
 }
